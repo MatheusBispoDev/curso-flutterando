56c1f17fd30ebd410a780c25197be6867a0f5925
coelho.matheus@outlook.com <MatheusBispoDev>
2021-08-19 11:37:18 -0300

    Termino de programacao assincrona/api

--- api/lib/app/controllers/home_controller.dart
+++ api/lib/app/controllers/home_controller.dart
@@ -1,25 +1,25 @@
 import 'package:api/app/models/todo_model.dart';
 import 'package:api/app/repositories/todo_repository.dart';
+import 'package:flutter/cupertino.dart';
 
 enum HomeState { start, loading, sucess, error }
 
 class HomeController {
   List<TodoModel> todos = [];
-
-  HomeState state = HomeState.start;
-
   final _repository;
+  //Adiciona a reatividade, notifica quando há alteração
+  final state = ValueNotifier<HomeState>(HomeState.start);
 
   HomeController([TodoRepository? repository])
       : _repository = repository ?? TodoRepository();
 
   Future start() async {
-    state = HomeState.loading;
+    state.value = HomeState.loading;
     try {
       todos = await _repository.fetchTodos();
-      state = HomeState.sucess;
+      state.value = HomeState.sucess;
     } on Exception {
-      state = HomeState.error;
+      state.value = HomeState.error;
     }
   }
 }
--- api/lib/app/home_page.dart
+++ api/lib/app/home_page.dart
@@ -1,15 +1,27 @@
 import 'package:api/app/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
 
   _sucess() {
     return ListView.builder(
-      itemCount: 10,
+      itemCount: controller.todos.length,
       itemBuilder: (context, index) {
+        var todo = controller.todos[index];
         return ListTile(
-          title: Text('item $index'),
+          leading: Checkbox(
+            value: todo.completed,
+            onChanged: (bool? value) {},
+          ),
+          title: Text(todo.title ?? ''),
         );
       },
     );
@@ -39,23 +51,41 @@ class HomePage extends StatelessWidget {
       case HomeState.start:
         return _start();
       case HomeState.loading:
-        return _loading()();
+        return _loading();
       case HomeState.error:
-        return _error()();
+        return _error();
       case HomeState.sucess:
-        return _sucess()();
+        return _sucess();
       default:
         return _start();
     }
   }
 
   @override
+  void initState() {
+    controller.start();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('List Todo\'s'),
-      ),
-      body: stateManager(HomeState.error),
-    );
+        appBar: AppBar(
+          title: Text('List Todo\'s'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.refresh_outlined),
+              onPressed: () {
+                controller.start();
+              },
+            ),
+          ],
+        ),
+        body: AnimatedBuilder(
+          animation: controller.state,
+          builder: (context, child) {
+            return stateManager(controller.state.value);
+          },
+        ));
   }
 }
