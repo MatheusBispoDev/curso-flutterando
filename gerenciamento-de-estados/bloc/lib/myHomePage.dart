8836e1469f4b6f0f088b58545e21d011b94439e5
coelho.matheus@outlook.com <MatheusBispoDev>
2021-08-19 17:17:04 -0300

    Bloc finalizado

--- gerenciamento-de-estados/bloc/lib/myHomePage.dart
+++ gerenciamento-de-estados/bloc/lib/myHomePage.dart
@@ -1,6 +1,63 @@
+import 'package:bloc/endereco_model.dart';
 import 'package:bloc/myHomePage_bloc.dart';
 import 'package:flutter/material.dart';
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  MyHomePageBloc bloc = MyHomePageBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloc'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextField(
+              onChanged: (value) {
+                bloc.input.add(value);
+              },
+              decoration: InputDecoration(
+                hintText: 'Digite seu CEP',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Center(
+              child: StreamBuilder<EnderecoModel>(
+                initialData: EnderecoModel(bairro: 'Sem bairro'),
+                stream: bloc.output,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      'Eror na pesquisa',
+                      style: TextStyle(color: Colors.red),
+                    );
+                  }
+
+                  EnderecoModel? model = snapshot.data;
+                  return Text('Bairro: ${model!.bairro}');
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -44,3 +101,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
--- gerenciamento-de-estados/bloc/lib/myHomePage_bloc.dart
+++ gerenciamento-de-estados/bloc/lib/myHomePage_bloc.dart
@@ -1,15 +1,29 @@
 import 'dart:async';
 
+import 'package:bloc/endereco_model.dart';
+import 'package:dio/dio.dart';
+
 class MyHomePageBloc {
-  int counter = 0;
+  //int counter = 0;
+  //final StreamController _streamController = StreamController();
+  //Sink get input => _streamController.sink; // Entrada de dados
+  //Stream get output => _streamController.stream; // Saída de dados
+  //void incrementCounter() {
+  //  counter++;
+  //  input.add(counter); //Passa o dado para o stream
+  //}
 
-  final StreamController _streamController = StreamController();
+  final StreamController<String> _streamController = StreamController<String>();
+  Sink<String> get input => _streamController.sink;
+  Stream<EnderecoModel> get output => _streamController.stream
+  .where((cep) => cep.length > 7)
+  .asyncMap((cep) => _searchCep(cep)); // Saída de dados
 
-  Sink get input => _streamController.sink; // Entrada de dados
-  Stream get output => _streamController.stream; // Saída de dados
+  String url(String cep) => 'https://viacep.com.br/ws/$cep/json/';
 
-  void incrementCounter() {
-    counter++;
-    input.add(counter); //Passa o dado para o stream
+  Future<EnderecoModel> _searchCep(String cep) async {
+    Response response = await Dio().get(url(cep));
+    return EnderecoModel.fromJson(response.data);
   }
+
 }
